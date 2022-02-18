f8b29f0327328d841acf36c5d0f4675ba1edb503
Virginie-ginie <87578256+Virginie-ginie@users.noreply.github.com>
2022-02-18 18:45:41 +0100

    Panier en cours

--- front/JS/cart.js
+++ front/JS/cart.js
@@ -1,31 +1,71 @@
-//---------------------Ajout des produits au panier----------------------------
 
-let couleurChoisie = document.getElementById("colors");
-let quantiteChoisie = document. getElementById("quantity");
-
-if (quantiteChoisie.value > 0 && quantiteChoisie.value <=100 && quantiteChoisie.value != 0 && couleurChoisie.value != 0) {{
-  
-  if (localStorage.getItem("panier")) {
-    
-    let productPanier = JSON.parse(localStorage.getItem("panier"));
-    console.log(productPanier);
-
-    //------------------Les variables pour les elements qu'on veux envoyer dans le panier-----------------
-
-    let idKanap = idProduct;
-    let couleurKanap = document.querySelector("#colors").value;
-    let quantiteKanap = document.querySelector("#quantity").value;
-  }
-}
-}
 
 //---------------------- Lire le Local storage pour recuperer le tableau de produit -------------------------------------
 
 
-//---------------------- Faire une boucle pour parcourir le tableau ---------------------
+//-----------------------j'initialise un tableau qui va acceuillir l'ensemble des articles-------------------------------
+let articles = [];
 
-        //-------------lire l'id dans le tableau-------------------
+
+//---------------------- Création d'une fonction qui ajoute chaque element du LocalStorage dans le tableau articles------------
+function recupStorage (){
+
+//---------------------- boucle qui envoi les elements dans le tableau articles-------------------------
+        for(let i = 0; i < localStorage.length; i++){
+           const key = localStorage.key(i);
+           //let article = JSON.parse(localStorage.getItem(key))
+           //articles.push(article);    
+        }
+        console.log(articles);
+};
+recupStorage()
+
+//---------------------- Faire une boucle pour parcourir le tableau ---------------------
+function recupArticle (){
+
+        articles.forEach(kanap => {
+                console.log(kanap);
+
+                console.log(kanap.name);
+
+                const cart = `
+                <article class="cart__item" data-id="{product-ID}" data-color="{product-color}">
+                <div class="cart__item__img">
+                  <img src="${kanap.imageUrl}"/>
+                </div>
+                <div class="cart__item__content">
+                  <div class="cart__item__content__description">
+                    <h2>${kanap.name}</h2>
+                    <p>${kanap.color}</p>
+                    <p>${kanap.price}€</p>
+                  </div>
+                  <div class="cart__item__content__settings">
+                    <div class="cart__item__content__settings__quantity">
+                      <p>Qté : </p>
+                      <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42">
+                    </div>
+                    <div class="cart__item__content__settings__delete">
+                      <p class="deleteItem">Supprimer</p>
+                    </div>
+                  </div>
+                </div>
+              </article>`
+              cart__items.innerHTML+=cart;
+        })
+        
+               
+        };
+        recupArticle()
+
+ //-------------lire l'id dans le tableau-------------------
+
+
+// ------------j'envoie chaque prix dans le tableau "price" -------------------------------
+
+
         //------------- chercher les info dans l'API --------------
+     
+
         //------------- faire le calcul final----------------------
         //------------- modifier la page HTML----------------------
 
--- front/JS/produits.js
+++ front/JS/produits.js
@@ -2,6 +2,7 @@
 let idResultatKanap = new URL(window.location.href).searchParams.get("id");
 
 //----------------Appel de l'API--------------------------------------
+
 let resultatkanap;
 const getResultatKanap = async () => {
   await fetch("http://localhost:3000/api/products/" + idResultatKanap)
@@ -18,18 +19,20 @@ const showProduct = async () => {
   //----------------- Création des balises------------------------------
 
   let imageKanap = document.querySelector(".item__img");
-  
+
   let couleurKanap = document.querySelector("#colors");
 
   //------------------ Ajout des balises--------------------------------
 
   //-------------Ajout de l'image-------------------------------------------
+
   let baliseImg = document.querySelector("img");
   baliseImg.setAttribute("src", resultatkanap.imageUrl);
   baliseImg.setAttribute("alt", resultatkanap.altTxt);
   imageKanap.appendChild(baliseImg);
 
   //--------------Ajout du nom ----------------------------------------------
+
   let baliseNom = document.getElementById("title");
   baliseNom.innerHTML = resultatkanap.name;
 
@@ -38,10 +41,12 @@ const showProduct = async () => {
   balisePrix.innerHTML = resultatkanap.price;
 
   //-------------Ajout de la description------------------------------------
+
   let baliseDescription = document.getElementById("description");
   baliseDescription.innerHTML = resultatkanap.description;
 
   //----------Ajout des couleurs -----------------------------------------
+
   for (let i = 0; i < resultatkanap.colors.length; i++) {
     let color = document.createElement("option");
     color.setAttribute("value", resultatkanap.colors[i]);
@@ -49,20 +54,65 @@ const showProduct = async () => {
     couleurKanap.appendChild(color);
   }
 
-//---------------------Ajout au local Storage --------------------------------
+  //---------------------Ajout au local Storage --------------------------------
+
+  //------------------ Recupération de l'element HTML quantité-----------
   
-  let bouton = document.getElementById("addToCart")
+  let quantity = document.querySelector("#quantity");
+  let quantityValue = quantity.value
+  let quantiteKanap = { "quantité":quantityValue};
+ 
+
+// //------------------ Recupération de l'element HTML couleur-----------
+
+  let color = document.querySelector("#colors");
+  let colorValue = color.value;
+
+  let couleurArticle = { "color ": colorValue };
+
+  let bouton = document.getElementById("addToCart");
   const product = resultatkanap;
-  
-  function addToPanier(key, value) {
+
+  function addToCart(key, value) {
     localStorage.setItem(key, value);
-  };      
-  
-  bouton.addEventListener('click',()=>{
-    addToCart(idResultatKanap,JSON.stringify(product));
-  })
+  }
+
+  bouton.addEventListener("click", () => {
+    addToCart(
+      idResultatKanap,
+      JSON.stringify(product) 
+      +
+        JSON.stringify(couleurArticle) +
+        JSON.stringify(quantiteKanap)
+      );
+      location.href = "cart.html"
+  });
 };
 showProduct();
 
 
+//---------------------Ajout des produits au panier----------------------------
 
+// let couleurChoisie = document.getElementById("colors");
+// let quantiteChoisie = document.getElementById("quantity");
+// let cart__items = document.getElementById("cart__items")
+// if (quantiteChoisie.value > 0 && quantiteChoisie.value <=100 && quantiteChoisie.value != 0 && couleurChoisie.value != 0) {{
+  
+//   if (localStorage.getItem("panier")) {
+    
+//     let productPanier = JSON.parse(localStorage.getItem("panier"));
+//     console.log(productPanier);
+
+//     let idKanap = idResultatKanap;
+//             let Kanapcouleur = document.querySelector("#colors").value;
+//             let kanapquantite = document.querySelector("#quantity").value;
+
+//             const resultFind = productCart.find(
+//                 (el) => el.idKanap === idResultatKanap && el.Kanapcouleur === Kanapcouleur);
+
+    
+//   }
+// }
+// }
+
+//------------------Les variables pour les elements qu'on veux envoyer dans le panier-----------------
